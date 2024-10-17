5196ae480b41c70dd084ee17035de278af5a68f0
Yves Renier <yves.renier@epfl.ch>
2024-10-17 10:57:24 +0200

    add contextlib.chdir definition for python <=3.11 compatibility.

--- src/fasttext_reducer/reduce_fasttext_models.py
+++ src/fasttext_reducer/reduce_fasttext_models.py
@@ -4,7 +4,24 @@ import errno
 import fasttext
 from fasttext.util import download_model, reduce_model
 from argparse import ArgumentParser
-from contextlib import chdir
+try:
+    from contextlib import chdir
+except ImportError:
+    from contextlib import AbstractContextManager
+    
+    class chdir(AbstractContextManager):
+        """Non thread-safe context manager to change the current working directory."""
+
+        def __init__(self, path):
+            self.path = path
+            self._old_cwd = []
+
+        def __enter__(self):
+            self._old_cwd.append(os.getcwd())
+            os.chdir(self.path)
+
+        def __exit__(self, *excinfo):
+            os.chdir(self._old_cwd.pop())
 
 
 def make_sure_path_exists(path, full_perm=True):
